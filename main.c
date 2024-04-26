55f2e136ef589612821f5eb404bd6ec6599f1d1f
Chiwi21 <alfredochiguano85@gmail.com>
2024-04-26 11:09:00 -0500

    hecho

---
+++ main.c
@@ -0,0 +1,8 @@
+ #include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  git config --global user.name "Chiwi21"
+  git config --global user.email "alfredochiguano85@gmail.com"
+  return 0;
+}
